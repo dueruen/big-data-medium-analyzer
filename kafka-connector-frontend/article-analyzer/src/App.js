30544750cebeb356340817142b8ca66bcdaf4ace
Victor Gram Thomsen <victor_gram@ymail.com>
2020-12-18 16:43:04 +0100

    React frontend publishing article objects to Kafka

---
+++ kafka-connector-frontend/article-analyzer/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ArticleAnalyzer from './components/arcticleAnalyzer/articleAnalyzer'
+
+function App() {
+  return (
+    <div className="App">
+      <ArticleAnalyzer></ArticleAnalyzer>
+    </div>
+  );
+}
+
+export default App;
---
+++ kafka-connector-frontend/article-analyzer/src/api/kafkaClient.js
@@ -0,0 +1,37 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'article-analyzer',
+    brokers: ['10.123.252.211:9092', '10.123.252.212:9092', '10.123.252.213:9092']
+  })
+
+const AnalyzingTopic = "grammefars_test"
+const producer = kafka.producer()
+
+export const publishNewArticle = async(articleData) => {
+    console.log(articleData)
+
+    const reader = new FileReader();
+    reader.readAsDataURL(articleData.articlePicture)
+    reader.onload = async function () {
+        // convert image file to base64 string
+        const b64WithMeta = reader.result;
+        //Cuts of metadata about file, leaving only the b64 string
+        const stringSplit = b64WithMeta.split(",")
+        const b64 = stringSplit[1]
+        
+        await producer.connect()
+        await producer.send({
+          topic: AnalyzingTopic,
+          messages: [
+            { 
+                title: articleData.title,
+                subtitle: articleData.subtitle,
+                readingTime: articleData.readingTime,
+                publication: articleData.publication,
+                image: b64
+            },
+          ],
+        })
+      }   
+}
